d6151564dbccbd0cd1f402a76eecd6538f9cc9c5
Rune <sungjong5k@naver.com>
2025-10-02 01:21:29 +0900

    feat: add GitHub Actions workflow for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
-import nextra from "nextra";
+import nextra from 'nextra';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
 const withNextra = nextra({});
 
 export default withNextra({
+  output: isDevelopment ? 'standalone' : 'export',
+  basePath: isDevelopment ? '' : '/25-26-study-js-deep-dive',
+  assetPrefix: isDevelopment ? '' : '/25-26-study-js-deep-dive',
+  images: {
+    unoptimized: true,
+  },
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/home",
+        source: '/',
+        destination: '/home',
         permanent: true,
       },
     ];
